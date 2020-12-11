d27f39550edc1990c12c6c02cf972cfa977719aa
Adam Walker <awalker@virtualmindset.com>
2020-12-10 19:49:55 -0500

    Fonts

--- install-nerd-font.sh
+++ install-nerd-font.sh
@@ -1,12 +1,14 @@
-#! /bin/bash
+#!/usr/bin/env bash
 # install SourceCodePro Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
 echo "[-] Download fonts [-]"
-cd /tmp
 echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip
-unzip SourceCodePro.zip -d ~/.local/share/fonts
+unzip -u SourceCodePro.zip -d fonts
 echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip
-unzip FiraMono.zip -d ~/.local/share/fonts
-fc-cache -fv
+unzip -u FiraMono.zip -d fonts
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip -u FiraCode.zip -d fonts
+# fc-cache -fv
 echo "done!"
