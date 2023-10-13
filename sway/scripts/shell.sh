f9cdd7ca9d7f8d5f0bbbaa83628010edd146bc57
Adam Walker <adam.walker@virtualmindset.com>
2023-10-13 15:22:45 -0400

    feat: use alacritty

--- sway/scripts/shell.sh
+++ sway/scripts/shell.sh
@@ -8,15 +8,16 @@
 # fi
 
 # foot -c "${USER_CONFIG:-"/usr/share/sway/templates/foot.ini"}" "$@" &
-#alacritty "$@" &
+# alacritty "$@" &
+alacritty
 #
 #/usr/bin/wezterm-gui start --cwd .
-
-if [ $(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true and .window_properties.class == "org.wezfurlong.wezterm").window_properties.class, select(.focused==true and .app_id =="org.wezfurlong.wezterm").app_id') ]; then
-	PANE_ID=$(wezterm cli list-clients --format json | jq '.[] | .focused_pane_id')
-	WORKDIR=$(wezterm cli list --format json | jq -r ".[] | select(.pane_id == $PANE_ID).cwd")
-	wezterm cli spawn --new-window --cwd ${WORKDIR#file://adam-nuc}
-	false
-else
-	wezterm
-fi
+#
+# if [ $(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true and .window_properties.class == "org.wezfurlong.wezterm").window_properties.class, select(.focused==true and .app_id =="org.wezfurlong.wezterm").app_id') ]; then
+# 	PANE_ID=$(wezterm cli list-clients --format json | jq '.[] | .focused_pane_id')
+# 	WORKDIR=$(wezterm cli list --format json | jq -r ".[] | select(.pane_id == $PANE_ID).cwd")
+# 	wezterm cli spawn --new-window --cwd ${WORKDIR#file://adam-nuc}
+# 	false
+# else
+# 	wezterm
+# fi
